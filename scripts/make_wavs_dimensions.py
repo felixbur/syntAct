a785353dabe823cbb316cff48c72d97f846888d0
felixbur <fxburk@gmail.com>
2022-03-01 18:03:12 +0100

    update

--- scripts/make_wavs_dimensions.py
+++ scripts/make_wavs_dimensions.py
@@ -48,7 +48,7 @@ for vi, voc in enumerate(constant.VOICES):
             sig, sr = af.read(path+name)
             if len(sig) < min_length:
                 """ There is quite a lot of stuff that might go wrong, emofilt has the tendency to make pho file unrenderable 
-                (eg. when phonemes become to shortn or because of vowel target over/undershoot).
+                (eg. when phonemes become too short or because of vowel target over/undershoot).
                 Also MARY does not support all German voices properly, e.g. uses phonene symbols that are not part of the voice.
                 So we simply ignore files that are too short or cause errors...
                 """
