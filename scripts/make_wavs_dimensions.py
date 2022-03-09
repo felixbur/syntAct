918eea95ce2d77a2f894f53060fd259173377b06
felixbur <fxburk@gmail.com>
2022-03-09 10:33:49 +0100

    update

---
+++ scripts/make_wavs_dimensions.py
@@ -0,0 +1,91 @@
+import  os
+import audiofile as af
+import audeer 
+import argparse
+import sys
+sys.path.append("./scripts")
+import constant
+import shared 
+import numpy as np
+
+# error messages should go here
+error_file = './synth_errors.txt'
+
+
+parser = argparse.ArgumentParser(description='Script to generate synthesized wav files from text.')
+parser.add_argument('--texts', help='The texts file', required=True)
+parser.add_argument('--wavs', help='The folder for the wav files', required=True)
+parser.add_argument('--of', help='The output wave list', required=True)
+parser.add_argument('--change_phrase', help='Whether the emotions get different phrases or each\
+     phrase in all emotions', action='store_true')
+parser.add_argument('--play', help='To play the wav files', action='store_true')
+parser.add_argument('--grades', help='Whether grades should be used', action='store_true')
+parser.add_argument('--random_grade', help='Whether grades should be randomly distributed', action='store_true')
+
+args = parser.parse_args()
+
+with open(args.texts) as f:
+    phrases = f.readlines()
+
+out_file = args.of
+play = args.play
+wav_folder = args.wavs
+
+phrases = [p.split('\t')[-1].strip() for p in phrases]
+
+# maximum number of generate wav files
+max_wave_num = 10000000
+min_length = 500
+index = 0
+path = audeer.mkdir(wav_folder)+'/'
+
+with open(out_file, 'a') as of:
+    of.write('file,emotion,speaker,gender,intensity\n')
+
+if args.grades:
+    arousal_dim =['aroused', 'relaxed']
+    if args.random_grade:
+        steps = np.random.normal(0, .8, 3)
+    else:
+        steps = [-.5,  0, .5, 1]
+
+    if args.change_phrase:
+        for vi, voc in enumerate(constant.VOICES):
+            for pi, phrase in enumerate(phrases):
+                if index>max_wave_num:
+                    exit()
+                step_index = pi % len(steps)
+                dim_index = int(pi / len(steps))
+                dim = arousal_dim[dim_index]
+                grade = steps[step_index]
+                index = shared.make_wav(voc, dim, grade, phrase, pi, index, path, play, out_file)
+    else:
+        for vi, voc in enumerate(constant.VOICES):
+            for pi, phrase in enumerate(phrases):
+                for dim in arousal_dim:
+                    for grade in steps:
+                        if index>max_wave_num:
+                            exit()
+                        step_index = pi % len(steps)
+                        dim_index = int(pi / len(steps))
+                        dim = arousal_dim[dim_index]
+                        grade = steps[step_index]
+                        index = shared.make_wav(voc, dim, grade, phrase, pi, index, path, play, out_file)
+else:
+    # our set of categorical emotions
+    emotions = constant.EMOTIONS
+    if args.change_phrase:
+        for vi, voc in enumerate(constant.VOICES):
+            for pi, phrase in enumerate(phrases):
+                if index>max_wave_num:
+                    exit()
+                emo_index = pi % len(emotions)-1
+                emo = emotions[emo_index]
+                index = shared.make_wav(voc, emo, -1, phrase, pi, index, path, play, out_file)
+    else:
+        for vi, voc in enumerate(constant.VOICES):
+            for pi, phrase in enumerate(phrases):
+                for ei, emo in enumerate(constant.EMOTIONS):
+                    if index>max_wave_num:
+                        exit()
+                    index = shared.make_wav(voc, emo, -1, phrase, pi, index, path, play, out_file)
