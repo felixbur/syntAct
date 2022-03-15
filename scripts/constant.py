2f6510977d187d0e1dca720fcdb35badce19a8f2
felixbur <fxburk@gmail.com>
2022-03-15 14:01:13 +0100

    update

--- scripts/constant.py
+++ scripts/constant.py
@@ -2,10 +2,11 @@ EMOFILT_PATH = '/home/felix/data/workspace/Emofilt/emofilt/'
 EMOFILT_CONFIG = '/home/felix/data/research/syntAct/emofilt_config/'
 MBROLA_DB_PATH = '/home/felix/bin/MARY4/lib/voices/'
 VOICES = ['de1', 'de2', 'de3', 'de4', 'de6', 'de7']
+SEXES = {'de1':'female', 'de2':'male', 'de3':'female', 'de4':'male', 'de6':'male', 'de7':'female'}
 #VOICES = ['de7']
 EMOTIONS = ['happy', 'angry', 'sad', 'neutral']
-AROUSALS =['aroused', 'relaxed']
-SEXES = {'de1':'female', 'de2':'male', 'de3':'female', 'de4':'male', 'de6':'male', 'de7':'female'}
+AROUSALS ={'high': 'aroused', 'low': 'relaxed'}
+VALENCES ={'high': 'positive', 'low': 'negative'}
 # Minimal sensible length for audio files (in bytes)
 MIN_LENGTH = 500
 # A file name to remember errors
