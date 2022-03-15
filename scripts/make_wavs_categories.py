5025d5a9882cfd23e223f719a69052f8848e55c1
felixbur <fxburk@gmail.com>
2022-03-15 11:46:51 +0100

    update

--- scripts/make_wavs_categories.py
+++ scripts/make_wavs_categories.py
@@ -14,6 +14,7 @@ error_file = './synth_errors.txt'
 
 parser = argparse.ArgumentParser(description='Script to generate synthesized wav files from text.')
 parser.add_argument('--texts', help='The texts file', required=True)
+parser.add_argument('--num', help='The number of wav files to be synthesized', required=True)
 parser.add_argument('--wavs', help='The folder for the wav files', default='synthesized_audio')
 parser.add_argument('--of', help='The output wave list', default='synth_out.csv')
 parser.add_argument('--change_phrase', help='Whether the emotions get different phrases or each\
@@ -28,12 +29,14 @@ with open(args.texts) as f:
 out_file = args.of
 play = args.play
 wav_folder = args.wavs
+num = int(args.num)
 
 phrases = [p.split('\t')[-1].strip() for p in phrases]
 
-# maximum number of generate wav files
-max_wave_num = 40
-min_length = 500
+# maximum number of generated wav files
+max_wave_num = num
+# minimum sensible length for wav file
+min_length = constant.MIN_LENGTH
 index = 0
 path = audeer.mkdir(wav_folder)+'/'
 
@@ -52,6 +55,7 @@ while index<max_wave_num:
             emo_index = phrase_index % len(emotions) - 1
             emo = emotions[emo_index]
             index = shared.make_wav_cat(voc, emo, phrase, phrase_index, index, path, play, out_file)
+            phrase_index += 1
     else:
         # else we use the same phrase for all voices and all emotion categories
         phrase = phrases[phrase_index]
