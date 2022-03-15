93be1c6703b997f7d870fb9de53f54ac56e1166c
felixbur <fxburk@gmail.com>
2022-03-15 13:41:01 +0100

    update

--- scripts/say_two_emo.py
+++ scripts/say_two_emo.py
@@ -8,24 +8,27 @@ import constant
 
 parser = argparse.ArgumentParser(description='Call emofilt to generate wav file from text.')
 parser.add_argument('--text', help='The text', required=True)
-parser.add_argument('--emo', help='The emotion', required=True)
+parser.add_argument('--emo1', help='The first emotion', required=True)
+parser.add_argument('--grade1', help='The grade of the first emotion [0:1]', default='0.5')
+parser.add_argument('--emo2', help='The second emotion', required=True)
+parser.add_argument('--grade2', help='The grade of the second emotion [0:1]', default='0.5')
 parser.add_argument('--voc', help='The voice', required=True)
 parser.add_argument('--wav', help='The wav file', required=True)
-parser.add_argument('--grade', help='The grade of the emotions [-1:1]', default='.5')
 parser.add_argument('--play', help='To play the wav file', action='store_true')
 
 args = parser.parse_args()
 
 voc = args.voc
 text = args.text
-emo = args.emo
+emo1 = args.emo1
+grade1 = float(args.grade1)
+emo2 = args.emo2
+grade2 = float(args.grade2)
 wav_file = args.wav
 pho_file = './tmp.pho'
 play_file = args.play
-grade = float(args.grade)
 
-# The weird thing is that MARY seems to diregard the voice sex (for de1 and de3), 
-# so the base F0 is always for men. 
+# The weird thing is that MARY seems to diregard the voice sex, so the base F0 is always for men. 
 # That's why we shift for females the mean F0 upwards
 femalize = False
 if voc == 'de1' or voc == 'de3':
@@ -40,12 +43,17 @@ os.system(cmd_make_pho)
 
 # then we do the emotional simulation
 cmd_emotionalize = f'java -jar {constant.EMOFILT_PATH}emofilt.jar \
--cf {constant.EMOFILT_CONFIG}emofiltConfig.ini -if {pho_file} -voc {voc} -e {emo} -of {pho_file}'
+-cf {constant.EMOFILT_CONFIG}emofiltConfig.ini -if {pho_file} -voc {voc} -e {emo1} -of {pho_file}'
 # add the grade if necessary (grade/intensity goes from 0 for neutral to 1 for double effect, 
 # with 0.5 being the "normal" grade)
-if grade != .5:
-	cmd_emotionalize += f' -gr {grade}'
-
+if grade1 != 0.5:
+	cmd_emotionalize += f' -gr {grade1}'
+os.system(cmd_emotionalize)
+# and once more for the second emotion
+cmd_emotionalize = f'java -jar {constant.EMOFILT_PATH}emofilt.jar \
+-cf {constant.EMOFILT_CONFIG}emofiltConfig.ini -if {pho_file} -voc {voc} -e {emo2} -of {pho_file}'
+if grade2 != 0.5:
+	cmd_emotionalize += f' -gr {grade2}'
 os.system(cmd_emotionalize)
 
 # lastly, we synthesize the pho file
