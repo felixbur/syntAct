efdb7ab272475bda0dac257b1b9d78393b5116e6
Felix Burkhardt <fburkhardt@audeering.com>
2022-05-06 11:25:37 +0200

    update

--- scripts/constant.py
+++ scripts/constant.py
@@ -1,10 +1,10 @@
-EMOFILT_PATH = '/home/felix/data/workspace/Emofilt/emofilt/'
-EMOFILT_CONFIG = '/home/felix/data/research/syntAct/emofilt_config/'
-MBROLA_DB_PATH = '/home/felix/bin/MARY4/lib/voices/'
+EMOFILT_PATH = '/data/fburkhardt/Emofilt/emofilt/'
+EMOFILT_CONFIG = '/data/fburkhardt/syntAct/emofilt_config/'
+MBROLA_DB_PATH = '/data/fburkhardt/bin/MARY4/lib/voices/'
 VOICES = ['de1', 'de2', 'de3', 'de4', 'de6', 'de7']
 SEXES = {'de1':'female', 'de2':'male', 'de3':'female', 'de4':'male', 'de6':'male', 'de7':'female'}
 #VOICES = ['de7']
-EMOTIONS = ['happy', 'angry', 'sad', 'neutral']
+EMOTIONS = ['happy', 'angry', 'sad', 'neutral', 'bored', 'scared']
 AROUSALS ={'high': 'aroused', 'low': 'relaxed'}
 VALENCES ={'high': 'positive', 'low': 'negative'}
 # Minimal sensible length for audio files (in bytes)
